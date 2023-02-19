fd661607e2b2521785a35009d2a02813aae84bc9
SonataGirl <mike@magic.org>
2023-02-20 00:22:28 +0300

    Initial Commit

---
+++ Quizz-ios13/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quizz-ios13
+//
+//  Created by Sonata Girl on 20.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
